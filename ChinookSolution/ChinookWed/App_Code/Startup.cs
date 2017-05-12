b45af5e196bb3159a8e53f50c8a31a9781161d51
hyan3 <2337915857@qq.com>
2017-05-11 18:08:46 -0700

    Created web site protect

---
+++ ChinookSolution/ChinookWed/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChinookWed.Startup))]
+namespace ChinookWed
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
