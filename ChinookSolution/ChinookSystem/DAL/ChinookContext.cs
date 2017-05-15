cb9fd70d742b677141be83043909b36b51362f10
hyan3 <2337915857@qq.com>
2017-05-14 18:59:27 -0700

    EF Day1

--- ChinookSolution/ChinookSystem/DAL/ChinookContext.cs
+++ ChinookSolution/ChinookSystem/DAL/ChinookContext.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Additional NameSpaces
+using System.Data.Entity;
+#endregion
+
 namespace ChinookSystem.DAL
 {
-    class ChinookContext
+    //This is an interanl class for security reasons
+    //Acess is restricted to within this class library project
+    //Inherits DbContext for Entity Framework, which requires
+    //     System.Data.Entity
+    internal class ChinookContext:DbContext
     {
+        //Pass the connection string name to the
+        //DbContext using :base()
+        // i.e. connection string name is "ChinookDB"
+        public ChinookContext() :base("ChinookDB")
+        {
+
+        }
     }
 }
